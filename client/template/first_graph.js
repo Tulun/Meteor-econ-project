e77d9d433b0df76068c6afbf157d9aa034b95243
Tulun <jaykiraly@gmail.com>
2016-02-25 16:39:27 -0800

    Graph renders, although incorrectly.

---
+++ client/template/first_graph.js
@@ -0,0 +1,106 @@
+Template.firstGraph.onRendered(function() {
+  var data = Data.find({ dataSetId: 'hpi'}).fetch();
+
+  //define constants, height/width
+  var A = Data.findOne({ dataSetId: 'hpi'})
+  console.log(new Date(A.Time))
+  console.log(Number(A.BC_Vancouver_Index));
+
+
+  var margin = {top: 20, right: 20, bottom: 30, left: 50},
+    width = 600 - margin.left - margin.right,
+    height = 400 - margin.top - margin.bottom;
+
+
+  //define scales and axes
+
+  var x = d3.time.scale()
+    .range([0, width]);
+
+  var y = d3.scale.linear()
+    .range([0, height])
+  
+  var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient('bottom');
+
+  var yAxis = d3.svg.axis()
+    .scale(y)
+    .orient('left');
+
+  var line = d3.svg.line()
+  .x(function(d) { return x(new Date(d.Time)); })
+  .y(function(d) { return y(Number(d.BC_Vancouver_Index)); })
+
+
+  
+
+  //define key function to bind elements to documents
+  
+
+  //define the SVG element by selecting the SVG via its id attribute
+  var svg = d3.select("#first_graph")
+    .attr('width', width + margin.left + margin.right)
+    .attr('height', height + margin.top + margin.bottom)
+    .append('g')
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+  // svg.append("g")
+  //   .attr("class", "x axis")
+  //   .attr("transform", "translate(0," + height + ")");
+
+  svg.append("g")
+    .attr("class", "y axis")
+    .append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 6)
+    .attr("dy", ".71em")
+    .style("text-anchor", "end")
+    .text('Price Index');
+  
+
+  //declare a Deps.autorun block
+  Deps.autorun(function(){
+
+      //perform a reactive query on the collection to get an array
+      var dataset = Data.find({dataSetId: 'hpi'}, {limit: 3}).fetch();
+
+      var paths = svg.selectAll('path.line')
+        .data([dataset]);
+
+
+
+      //update scale domains and axises
+      // var w = 600;
+      // var h = 300;
+
+      // var xscale = d3.scale.ordinal()
+      //   .rangeRoundBands([0,w], 0.05);
+      // var yscale = d3.scale.linear()
+      //   .range([0,h]);
+      
+      x.domain(d3.extent(dataset, function(d) { return new Date(d.Time); }));
+      y.domain(d3.extent(dataset, function(d) { return Number(d.BC_Vancouver_Index); }));
+
+      // //Define key function, to be used when binding data
+      // var key = function(d) {
+      //     return d._id;
+      // };
+
+      //select elements that correspond to documents
+
+      //handle new documents via enter()
+      paths.enter()
+        .append('path')
+        .attr('d', line);
+             
+
+      //handle updates to documents via transition()
+      paths.transition()
+      
+
+      //handle removed documents via exit()
+      paths.exit()
+          .remove();
+  });
+});
--- server/upload.js
+++ server/upload.js
@@ -1,20 +1,3 @@
-// Meteor.methods({
-//   parseUpload( data ) {
-//     check( data, Array );
-
-//     for (let i=0; i<data.length; i++) {
-//       let item = data[i],
-//         exists = USCrimeData.findOne({ State: item.State } );
-
-//       if (!exists) {
-//         USCrimeData.insert(item);
-//       } else { 
-//         console.warn( 'Rejected. This item already exists.')
-//       }
-//     }
-//   }
-// });
-
 Meteor.methods({
   parseUpload( data, dataSetId ) {
     check( data, Array );
