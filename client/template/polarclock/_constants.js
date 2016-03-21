42335db58dc80270142bc5bc06ea3f9456f359a8
Tulun <jaykiraly@gmail.com>
2016-03-20 21:31:31 -0700

    Polar clock 2 works. Polar clock 1 requires D3 alpha stuff.

---
+++ client/template/polarclock/_constants.js
@@ -0,0 +1,4 @@
+Template.polarClock.onRendered(function() {
+  // $.getScript("http://d3js.org/d3.v4.0.0-alpha.21.min.js");
+  $('#script').html('<script src="http://d3js.org/d3.v4.0.0-alpha.21.min.js"></script>')
+});
